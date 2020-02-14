d7d91bd5958ab80710436b4ac8aa48f510c4aefa
Rebecca <rebeccadavidsson3@gmail.com>
2020-02-14 19:31:26 +0100

    frame

--- converter/urls.py
+++ converter/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('home/', views.home_view, name='home'),
     path('register/', views.register_view, name='register'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- converter/views.py
+++ converter/views.py
@@ -1,28 +1,50 @@
 from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
 from django.shortcuts import render
-import csv, io
 from django.contrib.auth.decorators import permission_required
 
 
 def index(request):
     context = {}
-    return render(request, 'login.html', context)
+    # TODO
+
+    print("OPENING HOMEPAGE")
+
+    return render(request, 'home.html', context)
 
 
 def login_view(request):
-    context = {}
-    return render(request, 'login.html', context)
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # If user already exists, redirect to index
+        if user is not None:
+            login(request, user)
+            return redirect("index")
+
+        # TODO: Dit afmaken met registeren, model voor User aanmaken
+        return redirect("index")
+
+    return render(request, "login.html", {"message": "Incorrect name and/or password"})
 
 
 def register_view(request):
     context = {}
+    # TODO
     return render(request, 'register.html', context)
 
 
 def home_view(request):
     context = {}
-    # van https://html.crumina.net/html-olympus/17-FriendGroups.html
-    # ALS VOORBEELD
-    return render(request, 'test.html', context)
+    # TODO
+    return render(request, 'home.html', context)
+
+
+def logout_view(request):
+    logout(request)
+    return render(request, 'login.html', {"message": "Logged out."})
