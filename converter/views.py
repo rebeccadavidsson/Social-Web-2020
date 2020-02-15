8fe892110a6e6566cd7ba1afe49d14baf4e03711
samverh <43989279+samverh@users.noreply.github.com>
2020-02-15 18:15:35 +0100

    register user

--- converter/views.py
+++ converter/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.urls import reverse
 from django.contrib.auth.decorators import permission_required
 
 
@@ -19,6 +20,7 @@ def login_view(request):
 
     if request.method == "POST":
         username = request.POST["username"]
+        print(username)
         password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
 
@@ -26,17 +28,39 @@ def login_view(request):
         if user is not None:
             login(request, user)
             return redirect("index")
+        else:
+            return render(request, "login.html", {"message": "Invalid credentials."})
 
         # TODO: Dit afmaken met registeren, model voor User aanmaken
-        return redirect("index")
-
-    return render(request, "login.html", {"message": "Incorrect name and/or password"})
+        # return redirect("index")
+    return render(request, "login.html", {"message": None})
 
 
 def register_view(request):
-    context = {}
-    # TODO
-    return render(request, 'register.html', context)
+
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        if not username or not password or not email or not first_name or not last_name:
+            return render(request, "register.html", {"message": "Please fill in all fields."})
+        if User.objects.filter(username=username).exists():
+            return render(request, "register.html", {"message": "This username is already taken."})
+        else:
+            user = User.objects.create_user(
+                username=username, password=password, email=email)
+            user.first_name = first_name
+            user.last_name = last_name
+            user.save()
+            user = authenticate(request, username=username, password=password)
+            print("succesfully registered")
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+
+    return render(request, "register.html")
 
 
 def home_view(request):
@@ -46,5 +70,8 @@ def home_view(request):
 
 
 def logout_view(request):
-    logout(request)
-    return render(request, 'login.html', {"message": "Logged out."})
+    if request.user.is_authenticated:
+        logout(request)
+        return render(request, "login.html", {"message": "Logged out."})
+    else:
+        return render(request, "login.html", {"message": "You are already logged out."})
