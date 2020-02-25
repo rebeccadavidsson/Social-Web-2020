f214d994b2547fb8bcca424ee03b4989a9bf743a
Rebecca <rebeccadavidsson3@gmail.com>
2020-02-25 12:35:12 +0100

    omg Github ging helemaal mis

--- converter/static/js/scraper.js
+++ converter/static/js/scraper.js
@@ -9,9 +9,6 @@ async function scrape() {
   await page.goto('https://usc.uva.nl/sport/groepsfitness/');
 	await page.evaluate(()=>document.querySelector('#mod-schedule > div.schedule-select > div').click())
 	await page.waitFor(1000);
-
-	await page.evaluate(()=>document.querySelector('#mod-schedule > div.schedule-select > div').click())
-
 	await page.setViewport({width: 1000, height: 1500})
 	await page.screenshot({path: 'test.png'});
   await browser.close();
