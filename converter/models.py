423a2ab0d222aab19e861585f696c31165eb3821
samverh <43989279+samverh@users.noreply.github.com>
2020-02-25 13:43:49 +0100

    added models for courses, not working yet, structure ok?

--- converter/models.py
+++ converter/models.py
@@ -1,11 +1,35 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from datetime import datetime
+
+
+
+# NIKS WERKT AAAAAHHHH MAAR STRUCTUUR IS OKE??
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=64)
+
+
+class ScheduleItem(models.Model):
+    date = models.DateTimeField(auto_now_add=True, blank=True)
+    capacity = models.IntegerField(default=0)
+    full = models.BooleanField(default=False)
+    # participants = models.ManyToManyField(Profile, on_delete=models.PROTECT)
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=64)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    scheduled_courses = models.ManyToManyField(ScheduleItem, on_delete=models.CASCADE)
+    price = models.DecimalField(decimal_places=2, max_digits=4)
 
 
 class History(models.Model):
-    sportname = models.CharField(max_length=64)
-    teacher = models.CharField(max_length=64)  # Willen we dat dit ook een profiel wordt?
+    # sportname = models.CharField(max_length=64)
+    # teacher = models.CharField(max_length=64)  # Willen we dat dit ook een profiel wordt?
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
     rating = models.IntegerField(default=0)
     date = models.DateField(default=timezone.now)
 
@@ -15,4 +39,4 @@ class Profile(models.Model):
     following = models.ManyToManyField(User, related_name='followers')
     feedmessages = models.ManyToManyField(History, related_name="feedmessages")
     interests = models.CharField(max_length=64)  # Willen we hier ook een rating aan toevoegen?
-    photo = models.ImageField(upload_to = 'static/images', default = 'images/logo.png')
+    photo = models.ImageField(upload_to = "static/images", default = "images/logo.png")
