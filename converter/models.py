2a9324ff80c0653ec358e324e5a7785cb02aa50d
samverh <43989279+samverh@users.noreply.github.com>
2020-02-26 12:37:35 +0100

    added models for post and rating

--- converter/models.py
+++ converter/models.py
@@ -3,14 +3,26 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from datetime import datetime
 
+from django.db.models import IntegerField, Model
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 class History(models.Model):
     # sportname = models.CharField(max_length=64)
     # teacher = models.CharField(max_length=64)  # Willen we dat dit ook een profiel wordt?
     # course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    rating = models.IntegerField(default=0)
+
     date = models.DateField(default=timezone.now)
 
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = IntegerField(
+        default=3,
+        validators=[
+            MaxValueValidator(5),
+            MinValueValidator(0)
+        ])
 
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -29,6 +41,7 @@ class ScheduleItem(models.Model):
     name = models.CharField(max_length=64)
     date = models.DateTimeField(auto_now_add=True, blank=True)
     participants = models.ManyToManyField(Profile, related_name="participants")
+    rating = models.ManyToManyField(Rating, related_name="ratings")
 
     locations = [
         ("universum", "Universum"),
@@ -42,6 +55,12 @@ class ScheduleItem(models.Model):
         choices=locations,
         default="universum",
     )
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+
 
 #
 #
