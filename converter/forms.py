f2d2a5ee6b634670e3c288130d575d8c6e93345e
samverh <43989279+samverh@users.noreply.github.com>
2020-03-05 12:12:45 +0100

    let user delete profile

--- converter/forms.py
+++ converter/forms.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm
 from .models import Profile
 from crispy_forms.helper import FormHelper
+from django.contrib.auth.models import User
 from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit
 from django import forms
 
@@ -11,19 +12,8 @@ class ProfileForm(ModelForm):
         fields = ('interests', 'photo')
         template_name = "settings.html"
 
-    # def __init__(self, *args, **kwargs):
-    #     super(ProfileForm, self).__init__(*args, **kwargs)
-    #     self.helper = FormHelper()
-    #     self.helper.layout = Layout(
-    #         Fieldset(
-    #             'first arg is the legend of the fieldset',
-    #             'like_website',
-    #             'favorite_number',
-    #             'favorite_color',
-    #             'favorite_food',
-    #             'notes'
-    #         ),
-    #         ButtonHolder(
-    #             Submit('submit', 'Submit', css_class='button white')
-    #         )
-    #     )
+
+class ProfileDeleteForm(ModelForm):
+    class Meta:
+        model = User
+        fields = []
